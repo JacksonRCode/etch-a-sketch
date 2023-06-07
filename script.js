5548d9b4a2681052cb1f493b49f4f534f3a2020d
JacksonRCode <jackdareid@gmail.com>
2023-06-07 15:31:21 -0700

    Made grid functional
    Made it so that when you hover over an tile it changes to black,
    and you can change the scale of the grid using buttons.

--- script.js
+++ script.js
@@ -14,10 +14,52 @@ function createBtns() {
                 }
                 gridSize--;
             }
-            console.log(gridSize);
+            changeGridText();
         });
     });
 }
 
+function changeGridText() {
+    let sizeText = document.querySelector('.grid-dim-text');
+    sizeText.textContent = gridSize + 'x' + gridSize;
+
+    newGrid();
+}
+
+function makeTile() {
+    let dim = 800 / gridSize;
+    let tile = document.createElement('div');
+    tile.style.width = dim + 'px';
+    tile.style.height = dim + 'px';
+    tile.addEventListener('mouseover', () => {
+        tile.classList.add('success')
+    });
+
+    return tile;
+}
+
+function makeGrid() {
+    let container = document.querySelector('.grid');
+    let num = gridSize * gridSize;
+
+    for (let i=0; i<num; i++) {
+        let tile = makeTile();        
+        container.appendChild(tile);       
+    }
+}
+
+function newGrid() {   
+    let container = document.querySelector('.grid');
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+
+    makeGrid();
+}
+
 let gridSize = 16;
 createBtns();
+
+makeGrid();
+
+
